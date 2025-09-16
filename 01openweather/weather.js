7268c7160c0307ececce3068d7bd801380bb98de
310-0 <leo@shingu.ac.kr>
2025-09-16 10:32:42 +0900

    openai

--- 01openweather/weather.js
+++ 01openweather/weather.js
@@ -35,4 +35,4 @@ function getweather(){
     });
 }
 
-getweather()
+// getweather()
---
+++ 02openAI/openai.js
@@ -0,0 +1,37 @@
+key = "sk-XgBNm5RNrEz0GLw0DLK_FlU7vgTOhEoe7BDcjaCjVKT3BlbkFJXRwkQLiDqE8p9_zQOv5RzAPYAEbcQVmvTDurHZxRgA"
+
+function talk(){
+    // alert("getWeather 가 수행됨됨")
+
+    talkdata = {
+        "model": "gpt-5-nano",
+        "messages": [
+
+        {
+            "role": "user",
+            "content": "약속시간에 늦지않는 방법 10가지"
+         }
+    ]
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "https://api.openai.com/v1/chat/completions",
+        headers: {
+            "Content-Type": "application/json",  
+                "Authorization": "Bearer " 
+                },
+        data: JSON.stringify(talkdata),
+                        
+
+    }).done(function(response){
+      
+        console.log(response)
+        txtMsg.value = "성공"
+
+    }).fail(function(error){
+        console.log(error)
+        txtMsg.value = "실패 "
+    });
+}
+

9f62b58b3351a097203eb40a34bb966f565a03af
310-0 <leo@shingu.ac.kr>
2025-09-30 10:43:43 +0900

    image

--- 03googlevision/vision.js
+++ 03googlevision/vision.js
@@ -1,3 +1,4 @@
+imagestring = null
 function processFile(event){
     content = event.target.result;
     imagestring = content.replace('data:image/jpeg;base64,', '');
@@ -18,16 +19,33 @@ function uploadFiles(files){
 var googleAPIKey = ""
 function analyze(){
 
-    reqestData = 
+    
     // 요구하는 데이터를 JSON형식으로 지정
     // https://cloud.google.com/vision/docs/detecting-faces?hl=ko#vision_face_detection-drest
+    requestData = 
+        {
+            "requests": [
+                {
+                    "image": {
+                    "content": imagestring
+                        },
+                "features": [
+                    {
+                    "maxResults": 100,
+                    "type": "FACE_DETECTION"
+                    }
+                    ]   
+                }
+             ]
+        }       
+
 
     $.ajax({
         type: "POST",
-        url: "https://vision.googleapis.com/v1/images:annotate",
+        url: "https://vision.googleapis.com/v1/images:annotate?key=" + googleAPIKey,
         headers:{
             "Content-Type": "application/json",
-            "Authorization": "Bearer " + googleAPIKey
+            // "Authorization": "Bearer " + googleAPIKey
         },
         data: JSON.stringify(requestData)
 
